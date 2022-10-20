63444e98e9fdf4d306a29af82e807bff4397998a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-10-19 20:26:18 -0700

    Fix resizable

--- examples/add_fullscreen_layer.rs
+++ examples/add_fullscreen_layer.rs
@@ -1,5 +1,6 @@
 //! Add a fullscreen layer i.e. a view with zero margin and zero padding.
 pub fn main() {
+    use cursive::view::Resizable;
     let mut c = cursive::default();
     let view = cursive::views::TextView::new("Hello World");
     c.add_fullscreen_layer(view.full_screen());
